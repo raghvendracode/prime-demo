b1300d47133d211792a92ec9941960a1066e978f
Raghvendra <45680005+raghvendracode@users.noreply.github.com>
2019-01-28 14:50:33 +0530

    crud using reative form
    crud using reative form...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { MyServiceService } from './my-service.service';
-
-//import { ChangePasswordModalComponent } from './change-password-modal.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
- 
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-
+  title = 'app';
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,21 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyAegaQidehPGMWi6cOvMdwCXmq7SiYFel8",
-    authDomain: "crud-f695d.firebaseapp.com",
-    databaseURL: "https://crud-f695d.firebaseio.com",
-    projectId: "crud-f695d",
-    storageBucket: "crud-f695d.appspot.com",
-    messagingSenderId: "844504264425"
-  }
+  production: false
 };
 
 /*
- * In development mode, for easier debugging, you can ignore zone related error
- * stack frames such as `zone.run`/`zoneDelegate.invokeTask` by importing the
- * below file. Don't forget to comment it out in production mode
- * because it will have a performance impact when errors are thrown
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
